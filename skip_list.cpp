ddb9dedb4751a7cbabfa40b22050cfd80ef51ff4
GadiGar <gadi.gar@gmail.com>
2019-08-02 14:35:27 +0300

    added varying and random levels

--- skip_list.cpp
+++ skip_list.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <time.h>
 
 const int MAX_LEVEL = 4;
 const double prob = 0.5;
@@ -17,9 +18,11 @@ class Node {
 class Skip {
  private:
     Node* header;
+    Node* find_position(int val, int level);
+    int rand_level();
  public:
     Skip();
-    void insert_node(int value, int level);
+    void insert_node(int value);
     void print_list();
 };
 
@@ -27,15 +30,45 @@ Skip::Skip() {
     header = new Node(MAX_LEVEL, -1);
     for (int i = 0; i < MAX_LEVEL; ++i)
         (header->next).push_back(nullptr);
+    srand(time(NULL));
 }
 
-void Skip::insert_node(int value, int level) {
-    // TODO(gadi): fix this, just wanted to manually add after header
-    Node* node = new Node(level, value);
-    for (int i = 0; i < node->level; ++i) {
-        (node->next).push_back((header->next)[i]);
-        (header->next)[i] = node;
+Node* Skip::find_position(int val, int level) {
+    Node* pos = header;
+    while ((pos->next[level]) != nullptr)
+        if ((pos->next)[level]->value < val)
+            pos = (pos->next)[level];
+        else
+            break;
+    return pos;
+}
+
+int Skip::rand_level() {
+    int level = 0;
+    while (level < MAX_LEVEL-1) {
+        int my_rand = rand() % 100;
+        // std::cout << my_rand << '\t';
+        if (my_rand > static_cast<int>(prob * 100))
+            ++level;
+        else
+            break;
     }
+    std::cout << '\n';
+    return level+1;
+}
+
+void Skip::insert_node(int value) {
+    int level = rand_level();
+    Node* node = new Node(level, value);
+    Node* prev;
+    std::cout << "next val will be " << value << " with level " << level << '\n';
+    for (int i = 0; i < node->level; ++i) {
+        prev = find_position(value, i);
+        (node->next).push_back((prev->next)[i]);
+        (prev->next)[i] = node;
+    }
+    std::cout << "insert value " << value << " after " << prev->value
+        << " with level " << level << '\n';
 }
 
 void Skip::print_list() {
@@ -53,9 +86,9 @@ void Skip::print_list() {
 
 int main() {
     Skip sl;
-    sl.insert_node(10, 2);
-    sl.insert_node(20, 3);
-    sl.insert_node(30, 1);
+    for (int i = 0; i < 20; ++i) {
+        sl.insert_node(rand() % 100);
+    }
     sl.print_list();
     return 0;
 }
